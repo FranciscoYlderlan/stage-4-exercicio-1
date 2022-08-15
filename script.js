e6a74c0aa2fcbf44eea9948805c7c547744210f7
Francisco Ylderlan <108945231+FranciscoYlderlan@users.noreply.github.com>
2022-08-15 10:29:28 -0300

    Conceitos da linguagem Javascript

---
+++ script.js
@@ -0,0 +1,55 @@
+let numberOne = Number(prompt("Informe o primeiro número."));
+let numberTwo = Number(prompt("Informe o segundo número."));
+
+// let numberOne = 10;
+// let numberTwo = 2;
+
+const results = [];
+
+
+const sum = numberOne+numberTwo;
+const subtraction = numberOne-numberTwo;
+const multiplcation = numberOne*numberTwo;
+const division = numberOne/numberTwo;
+const rest = numberOne%numberTwo;
+
+const equals = numberOne===numberTwo;
+const even = (numberOne+numberTwo)%2 === 0;
+
+results.push({"description": "adição", "value": sum });
+results.push({"description": "subtração", "value": subtraction });
+results.push({"description": "multiplicação", "value": multiplcation });
+results.push({"description": "divisão", "value": division });
+results.push({"description": "resto", "value": rest });
+results.push({"description": equals? "iguais" : "diferentes", "value": equals });
+results.push({"description": even? "par" : "ímpar", "value": even });
+
+// console.log(results);
+
+function outputResult(description, value) {
+    if (description === "iguais" || description === "diferentes" ) {
+        return `Os números são ${description}.`;
+    }
+    if (description === "par" || description === "ímpar" ) {
+        return `A soma é ${description}.`;
+    }
+    return `O resultado do(a) ${description} foi: ${value}.`;
+}
+
+function valueFormatted (description,value) {
+    if (description === "iguais" || description === "diferentes" ||
+        description === "par" || description === "ímpar" ){
+        return value;
+    }
+    return  String((value).toFixed(2)).replace('.',',');
+}
+console.log(`O primeiro número informado foi: ${numberOne}
+O segundo número informado foi: ${numberTwo}
+
+`);
+
+results.forEach(result => {
+    result.value = valueFormatted(result.description,result.value);
+    // console.log(outputResult(result.description,result.value));
+    alert(outputResult(result.description,result.value));    
+});
